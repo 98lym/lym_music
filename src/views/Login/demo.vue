fd66be2559a58e3abb249fac871111470a6e57d8
liyiming <liyiming@szzhiju.cn>
2023-01-30 16:37:52 +0800

    回退测试

---
+++ src/views/Login/demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>测试</div>
+</template>
+<script>
+import { defineComponent, reactive, toRefs } from 'vue'
+export default defineComponent({
+  name: 'HHHH',
+  setup () {
+    const data = reactive({})
+    return {
+      ...toRefs(data)
+    }
+
+  }
+})
+</script>
+<style>
+
+</style>
